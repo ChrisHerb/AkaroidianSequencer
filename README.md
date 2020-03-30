d704d268b096c7a5133ce4c37c83dfd51a5bb216
ChrisHerb <chrisherb.sites@outlook.com>
2020-03-30 20:13:58 +0200

    Updates comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# AkaroidianSequencer
+# Akaroidian Sequencer
+by Christopher Herb
+
+This is a 3 channel algorithmic sequencer inspired by the Euclidean algorithm. 
+Each channel will generate a sequence (called shape) which is first defined by the assigned length. 
+By default it plays a note in the first position but also according to the other 
+shapes' length. For instance, if shape 1 gets assigned the length = 5, 
+shape 2 = 7 and shape 3 = 13 it will output the following sequences: 
+
+Shape 1: 1 0 0 0 0
+Shape 2: 1 0 0 0 0 1 0
+Shape 3: 1 0 0 0 0 1 0 1 0 0 0 0 0
+
+These sequences are now free running polyrhythms with the exception that 
+channel 1 resets after it has reached the defined measure length. Channel 2 will
+reset after 2 bars and channel 3 will reset after 4 bars of this measure. 
+The shapes can also be freely rotated.
+
+In Addition, 3 extra channels will ouput sequences derived through complex logic 
+from the first 3 channels.
+
+This sketch is a prototype meant to be run on an Arduino with native USB capabilities
+(Arduino Leonardo, Arduino Micro).
+It will need a Midi clock and start/stop signals to run and can be controlled 
+via Midi CC. It ouputs Midi on the following channels: 
+
+Channel 1 = shape 1
+Channel 2 = shape 2
+Channel 3 = shape 3
+Channel 4 = extra logic output 1
+Channel 5 = extra logic output 2
+Channel 6 = extra logic output 3
+
+The following CC values can be used to control the sequencer:
+
+CC1 = shape 1 length
+CC2 = shape 2 length
+CC3 = shape 3 length
+CC4 = shape 1 rotation
+CC5 = shape 2 rotation
+CC6 = shape 3 rotation
--- akaroidian.midi.ino
+++ akaroidian.midi.ino
@@ -1,47 +1,19 @@
 /**
- * AKAROIDIAN SEQUENCER
- * MIDI IMPLEMENTATION
- * by Christopher Herb
+ * Akaroidian Sequencer (Midi Implementation)
+ * Copyright (C) 2020  Christopher Herb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * This is a 3 channel algorithmic sequencer inspired by the Euclidean algorithm. 
- * Each channel will generate a shape which is first defined by the assigned length. 
- * By default it plays a note in the first position but also according to the other 
- * shapes' length. For instance, if shape 1 gets assigned the length = 5, 
- * shape 2 = 7 and shape 3 = 13 it will output the following sequences: 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Shape 1: 1 0 0 0 0
- * Shape 2: 1 0 0 0 0 1 0
- * Shape 3: 1 0 0 0 0 1 0 1 0 0 0 0 0
- * 
- * These sequences are now free running polyrhythms with the exception that 
- * channel 1 resets after it has reached the defined measure length. Channel 2 will
- * reset after 2 bars and channel 3 will reset after 4 bars of this measure. 
- * The shapes can also be freely rotated.
- * 
- * In Addition, 3 extra channels will ouput sequences derived through complex logic 
- * from the first 3 channels.
- * 
- * This sketch is a prototype meant to be run on an Arduino with native USB capabilities
- * (Arduino Leonardo, Arduino Micro).
- * It will need a Midi clock and start/stop signals to run and can be controlled 
- * via Midi CC. It ouputs Midi on the following channels: 
- * 
- * Channel 1 = shape 1
- * Channel 2 = shape 2
- * Channel 3 = shape 3
- * Channel 4 = extra logic output 1
- * Channel 5 = extra logic output 2
- * Channel 6 = extra logic output 3
- * 
- * The following CC values can be used to control the sequencer:
- * 
- * CC1 = shape 1 length
- * CC2 = shape 2 length
- * CC3 = shape 3 length
- * CC4 = shape 1 rotation
- * CC5 = shape 2 rotation
- * CC6 = shape 3 rotation
- * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "MIDIUSB.h"
